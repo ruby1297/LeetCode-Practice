822144e55adc7ddb58d292351273df6231af33b5
Yu <105330187+ruby1297@users.noreply.github.com>
2025-02-03 22:10:53 +0800

    Time: 0 ms (100.00%) | Memory: 29.6 MB (59.69%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,25 +1,25 @@
-// class Solution {
-// public:
-//     void rotate(vector<int>& nums, int k) {
-//         int numSize = nums.size();
-//         k = k % numSize;  
-
-//         reverse(nums.begin(), nums.end());        
-//         reverse(nums.begin(), nums.begin() + k);  
-//         reverse(nums.begin() + k, nums.end());   
-//     }
-// };
-
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int numSize = nums.size();
-        k = k % numSize; 
-        vector<int> temps(numSize);  
+        k = k % numSize;  
 
-        for(int i = 0; i < numSize; i++) {
-            temps[(i + k) % numSize] = nums[i];  
-        }
-        nums = temps;
+        reverse(nums.begin(), nums.end());        
+        reverse(nums.begin(), nums.begin() + k);  
+        reverse(nums.begin() + k, nums.end());   
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int numSize = nums.size();
+//         k = k % numSize; 
+//         vector<int> temps(numSize);  
+
+//         for(int i = 0; i < numSize; i++) {
+//             temps[(i + k) % numSize] = nums[i];  
+//         }
+//         nums = temps;
+//     }
+// };
